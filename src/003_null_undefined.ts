4cd8c10e707350468dc29d2b5567a1790019ce57
gustcomer <gustcomer@gmail.com>
2024-03-17 22:14:19 -0300

    feat: add 003_null_undefined.ts

---
+++ src/003_null_undefined.ts
@@ -0,0 +1,8 @@
+// What is the output of the following code?
+
+console.log(null == undefined);
+console.log(null === undefined);
+console.log(typeof(null));
+console.log(typeof(undefined));
+
+// true false object undefined
